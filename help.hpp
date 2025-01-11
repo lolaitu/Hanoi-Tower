fe314b147b19ea7ac458bc44a29f873951a5d58b
moua <moua@moua-Latitude-5480-of-lolaitu@linux-casser.fr>
2025-01-11 17:01:52 +0100

    fusion readme de github avec amelioration pc (help.hpp et previous input en gris) + mise au propre help.hpp

--- help.hpp
+++ help.hpp
@@ -1,7 +1,5 @@
 
 void print_help() {
-	// cout << "This is a CLI version of the Hanoi tower game\nThe goal is to move the entire tower from the left pole to the right pole\n\nYou can move a disk by pressing on your keyboard the letter of the pole where come the disk,\n and secondly the letter of the pole where you want it to go.\nYou can remove a input by pressing Backspace.\n\nA disk cannot go on top of a thinner disk\n\nFinally, if you want more challenge, you can type :\n  $>hanoi 5\nor more to add disk and with it difficulty\n\n";
-
 	cout << "Welcome to the CLI version of the Tower of Hanoi game!\n\n"
 		 << "The objective of the game is to move the entire tower from the left pole to the right pole,\n"
 	     << "following the rules of the Tower of Hanoi.\n\n"
